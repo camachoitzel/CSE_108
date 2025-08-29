44eda092398da1d7f498d3f50651e88fd14cdcd8
camachoitzel <camachoitzel101@gmail.com>
2025-08-28 18:53:21 -0700

    First day of class Python intro

---
+++ Week_1/hello_world.py
@@ -0,0 +1,24 @@
+# hello world
+print("hello world")
+
+# lists
+nums = [3,6,7,2,1]
+print(nums[0])
+
+for i in range(10):
+    nums.append(i)
+
+print(nums)
+
+hello10 = "hello" * 10
+print(hello10)
+
+nums = [3,6,7,2,1]
+nums2 = [1,5,7]
+
+print(nums + nums2)
+
+name = "John"
+age = 12
+
+print("Hello, %s! You are %d years old" %(name, age))
