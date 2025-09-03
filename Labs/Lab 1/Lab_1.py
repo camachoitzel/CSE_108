c2be07b8f4b5a91b1ca99abcf00ed1d443f0b182
camachoitzel <camachoitzel101@gmail.com>
2025-09-02 20:35:07 -0700

    CSE 108 Lab 1 work and first day work

---
+++ Labs/Lab 1/Lab_1.py
@@ -0,0 +1,13 @@
+# 1)	Write an adding program that does the following (10 points):
+# 	Ask the user to enter two or more numbers separated by spaces
+# 	Print the sum of all the numbers to the console
+# 	Throw an error if they do not enter at least two numbers or contain a string
+# 	Note: the numbers can be integers or decimals
+# 	Example: 
+# 	The user enters: 1 2 3 4 
+# 	The program prints: 10
+
+def adding(num1, num2):
+    pass
+
+
