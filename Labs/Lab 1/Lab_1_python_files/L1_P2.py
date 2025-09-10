16d16b3572492df9b28b0002eb623e9b70040c0a
camachoitzel <camachoitzel101@gmail.com>
2025-09-09 20:22:56 -0700

    revised problem 2 trying to follow provided pseudocode

--- Labs/Lab 1/Lab_1_python_files/L1_P2.py
+++ Labs/Lab 1/Lab_1_python_files/L1_P2.py
@@ -1,3 +1,4 @@
+import os
 # 2)	Write a punishment automation program in Python that does the following (15 points):
 # Ask the user to enter a sentence and the times a sentence should be repeated
 # The program should write the sentence (with a line break) the number of times specified by the user to a file called “CompletedPunishment.txt”
@@ -6,11 +7,14 @@
 # The user enters this for the number of repeats: 100 
 # The program should write “I will not sleep in class” 100 times to “CompletedPunishment.txt”.
 
-sentence = input("Please input a sentence:  ")
-num_of_reps = int(input("Please input a number: "))
+def is_valid_sentence(s):
+    if s 
+    
+# sentence = input("Please input a sentence:  ")
+# num_of_reps = int(input("Please input a number: "))
 
-try: 
-    with open("CompletedPunishment.txt", "w") as file:
-        for i in range(num_of_reps):
-            file.write(sentence + "\n")
-except ValueError:
+# try: 
+#     with open("CompletedPunishment.txt", "w") as file:
+#         for i in range(num_of_reps):
+#             file.write(sentence + "\n")
+# except ValueError:
