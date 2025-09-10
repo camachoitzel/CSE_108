ea220766ea5305e9d88649ec3ad4bb9182dcb925
camachoitzel <camachoitzel101@gmail.com>
2025-09-09 21:11:29 -0700

    Finished problem 2 of Lab 1

--- Labs/Lab 1/Lab_1_python_files/L1_P2.py
+++ Labs/Lab 1/Lab_1_python_files/L1_P2.py
@@ -8,7 +8,46 @@ import os
 # The program should write “I will not sleep in class” 100 times to “CompletedPunishment.txt”.
 
 def is_valid_sentence(s):
-    if s 
+    for char in s:
+        if char.isalpha():
+            return True
+    return False
+
+def punishment():
+    attempts = 0
+
+    while attempts < 3:
+        sentence = input("Enter a sentence: \n")
+
+        if is_valid_sentence(sentence):
+            break
+        else:
+            print("Invalid input, sentence not detected try again")
+            attempts += 1
+    
+    if attempts == 3:
+        print("Too many incorrect attempts. Exiting program...Goodbye")
+        return
+    
+    reps = int(input("Enter the number of times to repeat the sentence: \n"))
+
+    file_path = "CompletedPunishment.txt"
+
+    try:
+        with open("CompletedPunishment.txt", "w") as file:
+            for i in range(reps):
+                file.write(sentence + "\n")
+        
+        print(f"{sentence} has been written {reps} times to {file_path}")
+    
+    except IOError as e:
+        print(f"An error occurred while writing to the file: {e}")
+
+    
+
+
+punishment()
+
     
 # sentence = input("Please input a sentence:  ")
 # num_of_reps = int(input("Please input a number: "))
