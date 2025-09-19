a2359de30a4c13846806ff61aba596c1b004b341
camachoitzel <camachoitzel101@gmail.com>
2025-09-18 18:48:36 -0700

    Learning javascript

---
+++ Week_3/javascript_syntax.js
@@ -0,0 +1,8 @@
+
+<><p id="demo1"></p>
+<p id="demo2"></p>
+<script>
+    function myFunction() {document.getElementById("demo1").innerHTML = "Hello Dolly!"};
+    document.getElementById("demo2").innerHTML = "How are you?";
+</script></>
+
